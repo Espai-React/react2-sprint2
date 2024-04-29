f1b3ccda90e7955c393f46842c1031af39bc7364
llopfilms <llopfilms@gmail.com>
2024-04-29 23:05:14 +0200

    Ex.1.2 Operador ternari

--- js/main.js
+++ js/main.js
@@ -36,3 +36,46 @@ console.log('person.greet() -->', person.greet());
 //Ex.4 Funció fletxa amb setTimeout
 const holaEn3s = () => setTimeout(() => console.log('Hola en 3 segons'), 3000);
 holaEn3s();
+
+//Ex.1.2 Operador ternari
+console.log('-------- Ex.1.2 Operador ternari --------');
+
+//Ex.1 Operador ternari bàsic
+const potConduir = (edat) => (edat > 18 ? 'Pot conduir' : 'No pot conduir');
+console.log('potConduir(18) -->', potConduir(28));
+console.log('potConduir(15) -->', potConduir(15));
+
+//Ex.2 Ús amb operadors de comparació
+const determinaMesGran = (num1, num2) =>
+  num1 > num2
+    ? `${num1} és mes gran que ${num2}`
+    : `${num1} no es mes gran que ${num2}`;
+console.log('determinaMesGran(2, 3) -->', determinaMesGran(2, 3));
+console.log('determinaMesGran(3, 2) -->', determinaMesGran(3, 2));
+
+//Ex.3 Ús enllaçat d'operadors ternaris
+const trobarMaxim = (num1, num2, num3) =>
+  num1 > num2
+    ? num1 > num3
+      ? `${num1} és el màxim`
+      : `${num3} és el màxim`
+    : num2 > num3
+    ? `${num2} és el màxim`
+    : `${num3} és el màxim`;
+console.log('trobarMaxim(3, 2, 1) -->', trobarMaxim(3, 2, 1));
+console.log('trobarMaxim(3, 1, 2) -->', trobarMaxim(3, 1, 2));
+console.log('trobarMaxim(2, 3, 1) -->', trobarMaxim(2, 3, 1));
+console.log('trobarMaxim(2, 1, 3) -->', trobarMaxim(2, 1, 3));
+console.log('trobarMaxim(1, 2, 3) -->', trobarMaxim(1, 2, 3));
+
+//Ex.4 Operador ternari dins d'un bucle
+const parellOImparell = (arrayNum) => {
+  for (let i = 0; i < arrayNum.length; i++) {
+    console.log(
+      `El número ${arrayNum[i]} és ${
+        arrayNum[i] % 2 === 0 ? 'parell' : 'imparell'
+      }`
+    );
+  }
+};
+parellOImparell([11, 20, 34, 4, 15, 43, 27, 8, 92, 121]);
