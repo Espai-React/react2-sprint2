63ee48bdc1b9d521bfa46809936d9e77b238b8d1
llopfilms <llopfilms@gmail.com>
2024-05-27 01:56:17 +0200

    Exercici 5

--- sprint2.2 - ecommerce/js/shop.js
+++ sprint2.2 - ecommerce/js/shop.js
@@ -71,7 +71,7 @@ let products = [
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 let cart = [];
-
+let comptador = 0;
 let total = 0;
 
 // Exercise 1
@@ -79,18 +79,26 @@ function buy(id) {
   // 1. Loop for to the array products to get the item to add to cart
   let producte = products.find((element) => element.id === id);
   let producteCart = { ...producte };
+  producteCart.quantity = 0;
+  producteCart.subTotal = 0;
   // 2. Add found product to the cart array
   let indexProducteCart = cart.findIndex(
     (element) => element.id === producteCart.id
   );
   if (indexProducteCart != -1) {
     cart[indexProducteCart].quantity++;
+    cart[indexProducteCart].subTotal =
+      cart[indexProducteCart].price * cart[indexProducteCart].quantity;
   } else {
     producteCart.quantity = 1;
+    producteCart.subTotal = producteCart.price * producteCart.quantity;
     cart.push(producteCart);
   }
+  comptador++;
+  document.getElementById('count_product').innerHTML = comptador;
   console.log('cart -->', cart);
   console.table(cart);
+  console.log('comptador -->', comptador);
 
   // Calcular total carro
   calculateTotal();
@@ -99,7 +107,10 @@ function buy(id) {
 // Exercise 2
 function cleanCart() {
   cart.length = 0;
+  comptador = 0;
+  document.getElementById('count_product').innerHTML = comptador;
   console.log('cart -->', cart);
+  printCart();
 }
 
 // Exercise 3
@@ -111,9 +122,10 @@ function calculateTotal() {
   for (let producteCart of cart) {
     if (producteCart.subTotalWithDiscount)
       total += producteCart.subTotalWithDiscount;
-    else total += producteCart.price * producteCart.quantity;
+    else total += producteCart.subTotal;
   }
   console.log('total -->', total);
+  return total;
 }
 
 // Exercise 4
@@ -131,6 +143,7 @@ function applyPromotionsCart() {
                 100
             )) /
           100;
+        delete producteCart.subTotal;
       }
     }
   }
@@ -141,6 +154,26 @@ function applyPromotionsCart() {
 // Exercise 5
 function printCart() {
   // Fill the shopping cart modal manipulating the shopping cart dom
+  let cart_list = document.getElementById('cart_list');
+  cart_list.innerHTML = '';
+  let total_price = document.getElementById('total_price');
+  total_price.innerHTML = '';
+
+  for (let producteCart of cart) {
+    let fila = document.createElement('tr');
+    fila.innerHTML = `
+      <th scope="row">${producteCart.name}</th>
+      <td>${producteCart.price}</td>
+      <td>${producteCart.quantity}</td>
+      <td>${
+        producteCart.subTotal
+          ? producteCart.subTotal
+          : producteCart.subTotalWithDiscount
+      }</td>
+    `;
+    cart_list.append(fila);
+  }
+  total_price.innerHTML = calculateTotal();
 }
 
 // ** Nivell II **
